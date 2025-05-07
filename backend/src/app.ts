b0c834fdefa4c8c4deddc51e3ab6d4e6136e922a
khangerel14 <162080218+khangerel14@users.noreply.github.com>
2025-05-07 14:11:11 +0800

    Update app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -26,3 +26,6 @@ const PORT = process.env.PORT ?? 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+app.get('/', (req, res) => {
+  res.send('Welcome to the Cargo-Web API!'); // Or any response you want
+});
