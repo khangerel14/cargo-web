d8a96c410d188d6873de1e397d339690b200722f
khangerel Be-ochir <khangerelbeochir.com>
2025-06-28 01:26:22 +0800

    fix: main bg increased

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -45,8 +45,8 @@ export default function Home() {
         <div className='relative'>
           <Image
             src='/image.jpg'
-            width={200}
-            height={200}
+            width={1000}
+            height={1000}
             alt='bg'
             className='w-full h-screen object-cover z-10'
           />
